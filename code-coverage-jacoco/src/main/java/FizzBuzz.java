6a91a88276aa13f09a92c71dac4bcfe4fffdd4ab
eric egan <36870093+okapetanios@users.noreply.github.com>
2025-05-30 17:00:54 -0700

    Update FizzBuzz.java
    Signed-off-by: eric egan <36870093+okapetanios@users.noreply.github.com>

--- code-coverage-jacoco/src/main/java/FizzBuzz.java
+++ code-coverage-jacoco/src/main/java/FizzBuzz.java
@@ -20,7 +20,7 @@ public class FizzBuzz {
     }
 
     //  NEW LINES TO TRIGGER SONAR CLOUD ANALYSIS - MUST BE GREATER THAN 20 LINES OF NEW CODE
-     public String getFizzBuzzWord(int number) {
+     public String getizzBuzzWord(int number) {
         StringBuilder fizzBuzzWord = new StringBuilder();
 
         if (number % 3 == 0) {
@@ -33,7 +33,7 @@ public class FizzBuzz {
 
         return (fizzBuzzWord.length() == 0) ? null : fizzBuzzWord.toString();
     }
-     public String getFizzBuzzWord(int number) {
+     public String getFizzuzzWord(int number) {
         StringBuilder fizzBuzzWord = new StringBuilder();
 
         if (number % 3 == 0) {

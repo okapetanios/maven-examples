f598103eabf01c72b7a386ef02bbd33694fd126d
Eric Egan <eegan@linuxfoundation.org>
2025-05-31 18:40:39 -0700

    UT1: with extra methods in fizzbuzz
    Signed-off-by: Eric Egan <eegan@linuxfoundation.org>

--- code-coverage-jacoco/src/main/java/FizzBuzz.java
+++ code-coverage-jacoco/src/main/java/FizzBuzz.java
@@ -18,4 +18,33 @@ public class FizzBuzz {
 
         return (fizzBuzzWord.length() == 0) ? null : fizzBuzzWord.toString();
     }
+
+    //  NEW LINES TO TRIGGER SONAR CLOUD ANALYSIS - MUST BE GREATER THAN 20 LINES OF NEW CODE
+     public String getGizzBuzzWord(int number) {
+        StringBuilder fizzBuzzWord = new StringBuilder();
+
+        if (number % 3 == 0) {
+            fizzBuzzWord.append("Fizz");
+        }
+
+        if (number % 5 == 0) {
+            fizzBuzzWord.append("Buzz");
+        }
+
+        return (fizzBuzzWord.length() == 0) ? null : fizzBuzzWord.toString();
+    }
+     public String getHizzBuzzWord(int number) {
+        StringBuilder fizzBuzzWord = new StringBuilder();
+
+        if (number % 3 == 0) {
+            fizzBuzzWord.append("Fizz");
+        }
+
+        if (number % 5 == 0) {
+            fizzBuzzWord.append("Buzz");
+        }
+
+        return (fizzBuzzWord.length() == 0) ? null : fizzBuzzWord.toString();
+    }
+    //  END NEW CODE TO TRIGGER ANALYSIS BY SONARCLOUD.
 }
